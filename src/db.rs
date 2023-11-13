97d978c23a7bf9691cbed771193379530201cb37
Matej Buocik <matej.buocik@gmail.com>
2023-11-13 20:29:32 +0100

    DB Update

--- src/db.rs
+++ src/db.rs
@@ -5,16 +5,16 @@ const DB_PATH: &str = "./database"; // TODO config file
 
 #[derive(Debug)]
 pub struct Note {
-    id: i32,
+    id: i64,
     /// seconds since the UNIX epoch
     date: u64,
     text: String,
 }
 
 impl Note {
-    pub fn new(date: SystemTime, text: String) -> Note {
+    pub fn new(text: String) -> Note {
         let id = 0;
-        let date = date
+        let date = SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
             .unwrap()
             .as_secs();
@@ -37,11 +37,25 @@ pub fn connect() -> Result<Connection> {
     Ok(db)
 }
 
-pub fn add_note(db: Connection, note: Note) -> Result<()> {
+pub fn add_note(db: Connection, note: &mut Note) -> Result<()> {
     db.execute(
         "INSERT INTO notes (date, text) VALUES (?1, ?2)",
         (&note.date, &note.text),
     )?;
 
+    note.id = db.last_insert_rowid();
     Ok(())
+}
+
+pub fn get_notes(db: Connection) -> Result<Vec<Result<Note>>> {
+    let mut stmt = db.prepare("SELECT id, date, text from notes")?;
+    let notes = stmt.query_map((), |row| {
+        Ok(Note {
+            id: row.get(0)?,
+            date: row.get(1)?,
+            text: row.get(2)?,
+        })
+    })?;
+
+    Ok(notes.collect())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod db;
 
 fn main() {
-    println!("Hello, world!");
+    let database = db::connect().unwrap();
+    // let mut note = db::Note::new("Wassup".to_string());
+    // db::add_note(database, &mut note).unwrap();
+    for note in db::get_notes(database).unwrap() {
+        println!("{:?}", note.unwrap());
+    }
 }
